a7e34a8431e56ca048c516936434757b578e86ec
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-21 10:53:25 -0700

    Update footer.php

--- admin/layouts/footer.php
+++ admin/layouts/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="footer-inner">
         <div class="pull-left">
-            <span class="text-bold text-uppercase">Medical Management System</span>
+            <span class="text-bold text-uppercase">Art Gallery Management System</span>
         </div>
         <div class="pull-right">
             <span class="go-top"><i class="ti-angle-up"></i></span>
