1ff27a31c80ab729ee3af4a4f9ac6bfe70030073
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-09-23 06:18:27 -0700

    Add files via upload

--- admin/layouts/sidebar.php
+++ admin/layouts/sidebar.php
@@ -1,116 +1,128 @@
-<div class="sidebar app-aside" id="sidebar">
-    <div class="sidebar-container perfect scrollbar">
-          <nav>
-                <div class="navbar-title"><span>Main Navigative</span></div>
-                <ul class="main-navigation-menu">
-                    <li>
-                        <a href="dashboard.php">
+<div class="sidebar app-side" id="sidebar">
+    <div class="sidebar-container perfect scrollerbar">
+        <nav>
+             <div class="navbar-title"><span>Main Navigation</span></div>
+             <ul class="main-navigation-menu">
+                  <li>
+                       <a href="dashboard.php">
                             <div class="item-content">
                                 <div class="item-media"><i class="ti-home"></i></div>
                                 <div class="item-inner"><span class="title">Dashboard</span></div>
                             </div>
+                       </a>
+                  </li>
+                  <li>
+                        <a href="javascript:void(0)">
+                            <div class="item-content">
+                                <div class="item-media"><i class="ti-user"></i></div>
+                                <div class="item-inner"><span class="title">Artists</span></div>
+                            </div>
                         </a>
-                    </li>
-                    <li>
-                        <li>
-                            <a href="javascript:void(0)">
-                                <div class="item-content">
-                                    <div class="item-media"><i class="ti-user"></i></div>
-                                    <div class="item-inner"><span class="title">Doctors</span></div>
-                                </div>
-                            </a>
-                            <ul class="sub-menu">
-                                <li><a href="manage-doctors.php"><span class="title">Manage Doctors</span></a></li>
-                                <li><a href="create-doctor.php"><span class="title">Add Doctor</span></a></li>
-                                <li><a href="edit-doctor.php"><span class="title">Update Doctor</span></a></li>
-                                <li><a href="report-doctors.php"><span class="title">Doctor Reports</span></a></li>
+                        <ul class="sub-menu">
+                            <li><a href="manage-artists.php">Manage Artists</a></li>
+                            <li><a href="create-artist.php">Create Artist</a></li>
+                            <li><a href="edit-artist.php">Edit Artist</a></li>
+                            <li><a href="report-artist.php">Report</a></li>
+                        </ul>
+                  </li>
+                  <li>
+                        <a href="javascript:void(0)">
+                            <div class="item-content">
+                                <div class="item-media"><i class="ti-user"></i></div>
+                                <div class="item-inner"><span class="title">Exhibitions</span></div>
+                            </div>
+                        </a>
+                        <ul class="sub-menu">
+                            <li><a href="manage-exhibitions.php">Manage Exhibitions</a></li>
+                            <li><a href="create-exhibition.php">Create Exhibiton</a></li>
+                            <li><a href="edit-exhibition.php">Edit Exhibition</a></li>
+                            <li><a href="report-exhibitions.php">Report</a></li>
+                        </ul>
+                  </li>
+                  <li>
+                        <a href="javascript:void(0)">
+                            <div class="item-content">
+                                <div class="item-media"><i class="ti-user"></i></div>
+                                <div class="item-inner"><span>Gallery Visitors</span></div>
+                            </div>
+                        </a>
+                        <ul>
+                            <li><a href="manage-gallery-visitors.php">Manage Gallery Visitors</a></li>
+                            <li><a href="create-gallery-visitor.php">Create Gallery Visitor</a></li>
+                            <li><a href="edit-gallery-visitor.php">Edit Gallery visitor</a></li>
+                            <li><a href="report-gallery-visition.php">Report</a></li>
+                        </ul>
+                  </li>
+                  <li>
+                        <a href="javascript:void(0)">
+                            <div class="item-content">
+                                <div class="item-media"><i class="ti-user"></i></div>
+                                <div class="item-inner"><span>Artworks</span></div>
+                            </div>
+                            <ul>
+                                <li><a href="manage-artworks.php">Manage Artworks</a></li>
+                                <li><a href="create-artwork.php">Create Artwork</a></li>
+                                <li><a href="edit-artworks.php">Edit Artworks</a></li>
+                                <li><a href="report-artworks.php">Report</a></li>
                             </ul>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)">
-                                <div class="item-content">
-                                    <div class="item-media"><i class="ti-user"></i></div>
-                                    <div class="item-inner"><span class="title">Appointments</span><i class="icon-arrow"></i></div>
-                                </div>
-                            </a>
-                        </li>
-                        <ul class="sub-menu">
-                            <li><a href="manage-appointments.php"></a><span class="title">Manage Appointment</span></li>
-                            <li><a href="create-appointment.php"><span class="title">Add Appointment</span></a></li>
-                            <li><a href="edit-appointment.php"><span class="title">Update Appointment</span></a></li>
-                            <li><a href="appointment-reports.php"><span class="title">Appointment-reports.php</span></a></li>
-                        </ul>
-                    </li>
-                    <li>
+                        </a>
+                  </li>
+                  <li>
                         <a href="javascript:void(0)">
                             <div class="item-content">
                                 <div class="item-media"><i class="ti-user"></i></div>
-                                <div class="item-inner"><span class="title">Patients</span><i class="icon-arrow"></i></div>
+                                <div class="item-inner"><span>Sales</span></div>
                             </div>
+                            <ul>
+                                <li><a href="manage-sales.php">Manage Sales</a></li>
+                                <li><a href="create-sale.php">Create Sale</a></li>
+                                <li><a href="edit-sale.php">Edit Sale</a></li>
+                                <li><a href="report-sales.php">Reports</a></li>
+                            </ul>
                         </a>
-                        <ul class="sub-menu">
-                            <li><a href="manage-patients.php"><span class="title">Manage Doctors</span></a></li>
-                            <li><a href="create-patient.php"><span class="title">Add Patient</span></a></li>
-                            <li><a href="edit-patient.php"><span class="title">Update Patient</span></a></li>
-                            <li><a href="patient-reports.php"><span class="title">Patient-reports.php</span></a></li>
-                        </ul>
-                    </li>
-                    <li>
+                  </li>
+                  <li>
                         <a href="javascript:void(0)">
                             <div class="item-content">
                                 <div class="item-media"><i class="ti-user"></i></div>
-                                <div class="item-inner"><span class="title">Medical Records</span></div>
+                                <div class="item-inner"><span>Buyers</span></div>
                             </div>
+                            <ul>
+                                <li><a href="manage-buyers.php">Manage Buyers</a></li>
+                                <li><a href="create-buyers.php">Create Buyer</a></li>
+                                <li><a href="edit-buyer.php">Edit Buyer</a></li>
+                                <li><a href="report-buyers.php">Reports</a></li>
+                            </ul>
                         </a>
-                        <ul class="sub-menu">
-                            <li><a href="medical-records.php"><span class="title">Medical Records</span></a></li>
-                            <li><a href="create-medical-record.php"><span class="title">Add Medical Record</span></a></li>
-                            <li><a href="edit-medical-record.php"><span class="title">Update Medical Record</span></a></li>
-                            <li><a href="medical-reports.php"><span class="title">Medical Reports</span></a><li>
-                        </ul>
-                    </li>
-                    <li>
+                  </li>
+                  <li>
                         <a href="javascript:void(0)">
                             <div class="item-content">
                                 <div class="item-media"><i class="ti-user"></i></div>
-                                <div class="item-inner"><span class="title">Prescriptions</span></div>
+                                <div class="item-inner"><span>Exhibition Artworks</span></div>
                             </div>
+                            <ul>
+                                <li><a href="manage-exhibition-artworks.php">Manage Exhibition Artworks</a></li>
+                                <li><a href="create-exhibition-artwork.php">Create Exhibition Artwork</a></li>
+                                <li><a href="edit-exhibition-artwork.php">Edit Exhibition Artwork</a></li>
+                                <li><a href="report-exhibition-artworks.php">Report</a></li>
+                            </ul>
                         </a>
-                        <ul class="sub-menu">
-                            <li><a href="manage-prescription.php"><span class="title">Manage Prescriptions</span></a></li>
-                            <li><a href="create-prescription.php"><span class="title">Add Prescription></span></a></li>
-                            <li><a href="edit-prescription.php"><span class="title">Edit Prescription></span></a></li>
-                            <li><a href="prescription-reports.php"><span class="title">Prescription Reports</span></a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0)">
-                            <div class="item-content">
-                                <div class="item-media"><i class="ti-user"></i></div>
-                                <div class="item-inner"><span class="title">Billings</span></div>
-                            </div>
-                        </a>
-                        <ul class="sub-menu">
-                            <li><a href="manage-billings.php"><span class="title">Manage Billings</span></a></li>
-                            <li><a href="create-billing.php"><span class="title">Add Billing</span></a></li>
-                            <li><a href="edit-billing.php"><span class="title">Update billing></span></a></li>
-                            <li><a href="billing-reports.php"><span class="title">Billing Reports</span></a></li>
-                        </ul>
-                    </li>
-                    <!-- about page -->
-                    <li>
-                        <a href="javascript:void(0)">
-                            <div class="item-content">
-                                <div class="item-media"><i class="ti-user"></i></div>
-                                <div class="item-inner"><sapn class="title">Pages</sapn></div>
-                            </div>
-                        </a>
-                        <ul class="sub-menu">
-                            <li><a href="about-us.php"><span class="title">About Us</span></a></li>
-                            <li><a href="contact-us.php"><span class="title">Contact Us</span></a></li>
-                        </ul>
-                    </li>
-                </ul>
-          </nav>
+                  </li>
+                <!-- about page -->
+                <li>
+                    <a href="javascript:void(0)">
+                        <div class="item-content">
+                            <div class="item-media"><i class="ti-user"></i></div>
+                            <div class="item-inner"><span class="title">Pages</span></div>
+                        </div>
+                    </a>
+                    <ul class="sub-menu">
+                        <li><a href="about-us.php"><span class="title">About Us</span></a></li>
+                        <li><a href="contact-us.php"><span class="title">Contact Us</span></a></li>
+                    </ul>
+                </li>
+             </ul>
+        </nav>
     </div>
 </div>
