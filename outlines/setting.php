6cbfc6e80267039f98f75e9d0abc1b9d97037545
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-02 05:00:35 -0700

    Add files via upload

---
+++ outlines/setting.php
@@ -0,0 +1,140 @@
+<div id="setting" class="settings panel panel-default hidden-xs hidden-sm ">
+    <button ct-toggle="togle" data-toggle-class="active" data-toggle-target="#setting" class="btn btn-default">
+        <i class="fa fa-spin fa-gear"></i>
+    </button>
+    <div class="panel-heading">Style Selector</div>
+    <div class="panel-body">
+        <!-- Fixed Header -->
+         <div class="setting-box clearfix">
+              <span class="setting-title pull-left">Fixed Header</span>
+              <span class="setting-switch pull-right">
+                    <input id="fixed-header" type="checkbox" class="js-switch" >
+              </span>
+         </div>
+         <!-- Fixed Sidebar -->
+         <div class="setting-box clearfix">
+            <span class="setting-title pull-left">Fixed sidebar</span>
+            <span class="setting-switch pull-right">
+                <input id="fixed-sidebar" type="checkbox" class="js-switch">
+            </span>
+         </div>
+         <!-- Close Sidebar -->
+         <div class="setting-box clearfix">
+            <span class="setting-title pull-left">Close sidebar</span>
+            <span class="setting-switch pull-right">
+                <input id="closed-sidebar" type="checkbox" class="js-switch">
+            </span>
+         </div>
+         <!-- Fixed Footer -->
+         <div class="setting-box clearfix">
+            <span class="setting-title pull-left">Fixed Footer</span>
+            <span class="setting-switch pull-right">
+                <input id="fixed-footer" type="checkbox" class="js-switch">
+            </span>
+         </div>
+         <!-- THEME SWITCHER -->
+          <div class="colors-row setting-box">
+               <div class="color-theme theme-1">
+                    <div class="color-layout">
+                          <label>
+                                <input name="setting-them" type="radio" value="theme-1">
+                                <span class="ti-check"></span>
+                                <span class="split header">
+                                    <span class="color th-header"></span>
+                                    <span class="color th-collapse"></span>
+                                </span>
+                                <span class="split">
+                                    <span class="color th-sidebar"><i class="element"></i></span>
+                                    <span class="color th-body"></span>
+                                </span>
+                          </label>
+                    </div>
+               </div>
+               <div class="color-theme- theme-2">
+                    <div class="color-layout">
+                        <label>
+                            <input name="setting-theme" type="radio" value="theme-2">
+                            <span c="ti-check"></span>
+                            <span class="split header">
+                                <span class="color th-header"></span>
+                                <span class="color th-collape"></span>
+                            </span>
+                            <span class="split">
+                                <span class="color th-sidebar"><i class="element"></i></span>
+                                <span class="color th-body"></span>
+                            </span>
+                        </label>
+                    </div>
+               </div>
+          </div>
+          <div class="color-row setting-box">
+                <div class="color-theme theme-3">
+                    <div class="color-layout">
+                        <label>
+                            <input name="setting-theme" type="radio" value="theme-3">
+                            <span class="ti-check"></span>
+                            <span class="split header">
+                                <span class="color header"></span>
+                                <span class="color th-collapse"></span>
+                            </span>
+                            <span class="split">
+                                <span class="color th-sidebar"><i class="element"></i></span>
+                                <span class="color th-body"></span>
+                            </span>
+                        </label>
+                    </div>
+                </div>
+                <div class="color-theme them-4">
+                    <div class="color-layout">
+                        <label>
+                            <input name="setting-theme" type="radio" value="theme-4">
+                            <span class="ti-check"></span>
+                            <span class="split header">
+                                <span class="color th-header"></span>
+                                <span class="color th-collapse"></span>
+                            </span>
+                            <span class="split">
+                                <span color="color th-sidebar"><i class="element"></i></span>
+                                <span class="color th-body"></span>
+                            </span>
+                        </label>
+                    </div>
+                </div>
+          </div>
+          <div class="colors-row setting-box">
+               <div class="color-theme theme-5">
+                    <div class="color-layout">
+                        <label>
+                            <input name="setting-theme" type="radio" value="theme-5">
+                            <span class="ti-check"></span>
+                            <span class="split header">
+                                <span class="color th-header"></span>
+                                <span class="colors th-collapse"></span>
+                            </span>
+                            <span class="split">
+                                <span class="color th-sidebar"><i class="element"></i></span>
+                                <span class="color th-body"></span>
+                            </span>
+                        </label>
+                    </div>
+               </div>
+               <div class="color-theme theme-6">
+                    <div class="color-layout">
+                        <label>
+                            <input name="setting-theme" type="radio" value="theme-6">
+                            <span class="ti-check"></span>
+                            <span class="split header">
+                                <span class="color th-header"></span>
+                                <span class="color th-collapse"></span>
+                            </span>
+                            <span class="split">
+                                <span class="color th-sider"><i class="element"></i></span>
+                                <span class="color th-body"></span>
+                            </span>
+                        </label>
+                    </div>
+               </div>
+          </div>
+          <!-- END THEME -->
+    </div>
+</div>
