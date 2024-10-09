47ef7091ef3c67c464859ba08487ec030ba8f497
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-09 14:14:07 -0700

    Add files via upload

--- outlines/sidebar.php
+++ outlines/sidebar.php
@@ -1,1 +1,82 @@
-sidebar
+<div id="sidebar" class="sidebar app-aside" >
+	<div class="sidebar-container perfect-scrollbar">
+		<nav>						
+			<!-- start: MAIN NAVIGATION MENU -->
+			<div class="navbar-title">
+				<span>Main Navigation</span>
+			</div>
+			<ul class="main-navigation-menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-home"></i></div>
+							<div class="item-inner"><span class="title"> Dashboard </span></div>
+						</div>
+					</a>
+				</li>				
+				<li>
+					<a href="manage_artists.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Artists </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_artworks.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Artworks </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_exhibitions.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title"> Manage exhibitions </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_exhibition_artworks.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-list"></i></div>
+							<div class="item-inner">
+								<span class="title"> Manage exhibition_artworks </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="manage_gallery_visitors.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-pencil-alt"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Gallery Visitors </span>
+							</div>
+						</div>
+					</a>
+				</li>
+                <li>
+					<a href="manage_buyerss.php">
+						<div class="item-content">
+							<div class="item-media"><i class="ti-pencil-alt"></i></div>
+							<div class="item-inner">
+								<span class="title">Manage Buyers </span>
+							</div>
+						</div>
+					</a>
+				</li>
+			</ul>
+			
+			<!-- end: CORE FEATURES -->						
+		</nav>
+	</div>
+</div>
