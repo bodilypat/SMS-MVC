661a63e305b189904011a3941aac69abec59d9b9
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-10-17 07:30:05 -0700

    Add files via upload

--- admin/sales/add_sale.php
+++ admin/sales/add_sale.php
@@ -1,40 +1,101 @@
-@extends('layout.app')
+<?php
+    session_start();
+    error_reporting(0);
+    include('../configure/dbconnect.php');
 
-@section('content')
-    <div class="content">
-        <h1>Create Sale</h1>
-        <form action="{{ route('sales.store') }}" method="POST">
-            @csrf
-                <div class="form-group">
-                    <label for="artwork_id">Artwork</label>
-                    <select name="artwork_id" id="artwor_id" class="form-control">
-                        @foreach ($arworks as $artwork)
-                            <option value="{{ $artwork->id}}">{{ $artwork->title }}</option>
-                        @endforeach
-                    </section>
-                    @error('artwork_id')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="buyer_id">Buyer</label>
-                    <select name="buyer_id" id="buyer-id" class="form-control">
-                        @foreach($buyers as $buyer)
-                            <option value="{{ $buyer->id }}">{{ $buyer->name }}</option>
-                        @endforeach
-                    </section>
-                    @error('buyer_id')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="amount">Amount</label>
-                    <input type="text" name="amount" id="amount" class="form-control" value="{{ old('sale_date') }}" requried>
-                    @error('sale_date')
-                        <div class="alert alert-danger">{{ message }}</div>
-                    @enderror
-                </div>
-                <button type="submit" class="btn btn-primary">Record Sale</button>
-        </form>
-    </div>
-@endsection
+    if(strlen($_SESSION['artid'] == 0)){
+        header('location:logout.php');
+    } else {
+        if(isset($_POST['submit'])) {
+            $_POST['fullname'];
+            $contactno = $_POST['phone'];
+            $email = $_POST['email'];
+            $address = $_POST['address'];
+           
+            $addB = mysqli_query($dbcon,"INSERT INTO sales(fullname,artwork_id,amount, sale_date, )
+                                               VALUES('$artName','$artMobile','$artEmail','$awardDetial','$artPic' ");
+                if($addArt) {
+                    echo "<script>alert('Artist details has been added.');</script>";
+                    echo "<script>window.location.href='manageArtist.php'</script>";
+                } else {
+                    echo "<script>alert('Something went wrong. Please try again.');</script>";
+                }
+            }
+        }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Add Artist | Gallery Management System</title>
+        <!-- custom styles -->
+        <link rel="stylesheet" href="../assign/css/style.css">
+        <link rel="stylesheet" href="../assign/css/style-responsive.css">
+    </head>
+<body>
+    <section id="container">
+        <?php include_once('../configure/header.php');?>
+        <?php include_once('../configure/sidebar.php'); ?>
+        <section class="main-content">
+             <section class="wrapper">
+                  <div class="row">
+                      <div class="col-lg-12">
+                           <h3 class="page-header"><i class="fa fa-file-text-o"></i>Add Artists</i></h3>
+                           <ol class="breadcrumb">
+                                 <li><i class="fa fa-home"></i><a href="dashboard">Home</li>
+                                 <li class="icon_document_alt">Artist</li>
+                                 <li class="fa fa-file-text-0">Add Artist</li>
+                           </ol>
+                      </div>
+                  </div>
+                  <div class="row">
+                       <div class="col-lg-12">
+                            <section class="panel">
+                                <header class="panel-heading">Add Artist Details</header>
+                                <div class="panel-body">
+                                    <form class="form-horizontal" method="post" action="" enctype="multipart/form-data">
+                                        <div class="form-group">
+                                            <label for="ArtistName">Name</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" id="artistName" name="artistName" class="form-control" required="true">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-2" for="MobileNumber">Mobile Number</label>
+                                            <div class="col-sm-10">
+                                                <input type="text" id="mobileNumberr" name="mobileNumber" class="form-control" required="true">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-2" for="Email">Email</label>
+                                            <div class="col-sm-10" >
+                                                <input type="email" id="email" name="email" class="form-control" required="true">
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-s" for="AwardDetails">Award Details</label>
+                                            <div class="col-sm-10">
+                                                <textarea class="form-control" name="awarddetails" requried="true"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-sm-2" for="ProfilePic">Profile Picture</label>
+                                            <div class="col-sm-10">
+                                                <input type="file" class="form-control" name="images" value="" required="true">
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </section>
+                       </div>
+                  </div>
+             </section>
+        </section>
+        <!-- FOOTER -->
+        <?php include_once('../define/footer.php');?>
+    </section>
+    <!-- custom javascript -->
+    <script src="../assign/js/form-component.js"></script>
+    <script src="../assign/js/scripts.js"></script>
+</body>
+</html>
+<?php    } ?>
