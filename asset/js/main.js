5aa764960e63c7faf36fcc396ef6fdf133a6ac80
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-11-11 14:12:38 -0800

    Add files via upload

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,44 +1,48 @@
-$(function (){
-    $('.gIquit').click(function () {
-        $('.galleryModal').css({'transform' : 'scale(0)' })
-        $('.galleryShadow').fadeOut()
+$(function() {
+    $('.gIquit').click(function() {
+        $('.gallery-model').css({ 'transform': 'scale(0)' })
+        $('.gallery-shadow').fadeOut();
     })
     $('#slideshow-icon').click(function () {
         galleryNavigate($('.image'), 'opened')
-        $('.galleryModal').css({ 'transform' : 'scale(1)' })
-        $('.galleryShadow').fadeIn()
+        $('.gallery-modal').css({ 'transform': 'scale(1)' })
+        $('.gallery-shadow').fadeIn();
     })
 
-    let galleryNavigate
-    let galleryNew
-    let galleryNewImg
-    let galleryNewText
-    $('.gIleft').click(function () {
-        galleryNew = $(galleryNav).prev()
-        galleryNavigate(galleryNew,'list')
-    })
+    let gallery_nav
+    let gallery_new
+    let gallery_new_img
+    let gallery_new_text 
+
     $('.gIright').click(function () {
-        galleryNew = $(galleryNav).next()
-        galleryNavigate(galleryNew,'first')
+        gallery_new = $(gallery_nav).prev()
+        galleryNavigate(gallery_new,'last')
     })
 
-    function galleryNavigate(gDate, direction) {
-        galleryNewImg = gDate.children('img').attr('src')
-        if(typeof galleryNewImg !== 'underfined') {
-            galleryNav = gData 
-            $('.galleryModal img').attr('src', galleryNewImg)
-        } else {
-            gDate = $('.image:', + direction)
-            galleryNav = gData
-            galleryNewImg = gData.children('img').attr('src')
-            $('.galleryModal img').attr('src', galleryNewImg)
+    $('gIleft').click(function() {
+        gallery_new = $(gallery_nav).prev()
+        galleryNavigate(gallery_naw,'first')
+    })
+
+    function galleryNavigate(gData, direction) {
+        gallery_new_img = gData.children('img').attr('src')
+        if(typeof gallery_new_img !== "undefined") {
+            gallery_nav = gData
+            $('.gallery_modal img').attr('src', gallery_new_img)
         }
-        galleryNewText = gData.children('img').attr('data-text')
-        if(typeof galleryNewText !== 'Underfined') {
-            $('.galleryModal .galleryContainer .galleryText').remove()
-            $('.galleryModal .galleryContainer').append('<div class="galleryText">' + galleryNewText + '</div>')
-        } else {
-            $('.galleryModal .galleryContainer .galleryText').remove();
+        else {
+            gData = $('.image:' + direction)
+            gallery_nav = gData
+            gallery_new_img = gData.children('img').attr('src')
+            $('.gallery_modal img').attr('src', gallery_new_img)
+        }
+        gallery_new_text = gData.children('img').attr('data-text')
+        if(typeof gallery_new_text !== "undefined") {
+            $('.gallery_modal .galelry_container .gallery_text').remove()
+            $('.gallery_modal .gallery_container').append('<div class="gallery_text">' + gallery_new_text + '</div>')
+        }
+        else {
+            $('.gallery_modal .gallery_container .gallery_text').remove()
         }
     }
 })
