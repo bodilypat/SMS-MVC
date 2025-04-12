e513e3e634f8b1b7212036f20b1adba9c9b36394
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-04-12 15:00:27 -0700

    Add files via upload

--- database/dbgallery.sql
+++ database/dbgallery.sql
@@ -10,23 +10,30 @@ CREATE TABLE users(
 CREATE TABLE artists(
     artist_id INT AUTO_INCREMENT PRIMARY KEY,
     frist_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    birth_data DATE,
-    death_date DATE,
+	last_name VARCHAR(100) NOT NULL,
+	birth_data DATE,
+	death_date DATE DEFAULT NULL,
+	biography TEXT,
     biography TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    INDEX idx_name (first_name, last_name),
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	INDEX idx_name (first_name, last_name),
 );
 
 CREATE TABLE artworks(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMA	CRERY KEY,
     title VARCHAR(255) NOT NULL,
     artist_id INT,
     description TEXT,
-    price DECIMAL(10,2),
-    image_path VARCHAR(255),
+	medium VARCHAR(100),
+	year_created year,
+	price DECIMAL(10,2) NOT NULL,
+	status ENUM('available','sold','on load' ) DEFAULT 'available',
+	image_path VARCHAR(255),
     created_at DATETIME CURRENT_TIMESTAMP,
+	updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON DEDATE CURRENT_TIMESTAMP,
     FOREIGN KEY(artist_id) REFERENCES artists(id);
+	INDEX (artist_id)
 );
 
 CREATE TABLE exhibitions(
@@ -60,7 +67,7 @@ CREATE TABLE sales(
     FOREIGN KEY (artwotk_id) REFERENCES artworks(id) ON DELETE CASCADE,
     FOREIGN KEY (user_id) FOREIGN users(id) ON DELETE CASCADE
 );
-
+ 
 CREATE TABLE customers(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -72,18 +79,17 @@ CREATE TABLE customers(
 );
 
 CREATE TABLE inventory (
-	product_id INT AUTO_INCREMENT PRIMARY KEY,     -- unique identifier for each product
-	sku VARCHAR(50) NOT NULL UNIQUE,               -- stock keeping unit, a unique code for ech product 
-	product_name VARCHAR(255) NOT NULL,            -- name of product 
-	description TEXT,                              -- description oft he product
-	category VARCHAR(100),                         -- category of product
-	quantity INT DEFAULT 0,                        -- quantity available oft he product
-	price DECIMAL(10,2) NOT NULL,                  -- price per unit of the product
-	supplier_id INT,							   -- supplier ID
-	reorder_level INT DEFAULT 10,                  -- minimum stock level to trigger a reorder
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,           -- date when the product was added to the inventory
-	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRRENT_TIMESTAMP,  -- date when product details 
+	product_id INT AUTO_INCREMENT PRIMARY KEY,    
+	sku VARCHAR(50) NOT NULL UNIQUE,               
+	product_name VARCHAR(255) NOT NULL,            
+	description TEXT,                             
+	category VARCHAR(100),                        
+	quantity INT DEFAULT 0,                       
+	price DECIMAL(10,2) NOT NULL,                  
+	supplier_id INT,							   
+	reorder_level INT DEFAULT 10,                 
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,           
+	update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRRENT_TIMESTAMP,  
 	FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
 );
-
 
