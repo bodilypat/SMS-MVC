2584075a29ce3d7ca91a31eb0cd251e7b9c47957
bodilypat <139784676+bodilypat@users.noreply.github.com>
2025-07-11 13:13:40 -0700

    Create create_users_table.php

---
+++ backend/database/migrations/create_users_table.php
@@ -0,0 +1,32 @@
+<?php
+	
+	/* Backend/database/migration/create_users_table.php */
+	
+	use Illuminate\Database\Migrations\Migration;
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Support\Facades\Schema;
+	
+	return new class extends Migration {
+		public function up(): void
+		{
+			Schema::create('users', function (Blueprint $table) {
+				$table->id();
+				$table->string('name');
+				$table->string('email')->unique();
+				$table->timestamp('email_verified_at')->nullable();
+				$table->string('password');
+				$table->rememberToken();
+				
+				/* CUSTOM FIELD FOR USER TYPE */
+				$table->enum('role', ['admin','sales','marketing'])->default('sales');
+				
+				$table->timestamps();
+			});
+		}
+		
+		public function down(): void 
+		{
+				Schema::dropIfExist('users');
+		}
+	};
+	
